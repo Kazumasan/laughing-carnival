79812ee234a48316cccbc929f296a7842f23eaa1
Kazumasan <19864466+Kazumasan@users.noreply.github.com>
2024-05-30 15:01:12 +0200

    Repair anime fetch & implement studio fetch

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -2,6 +2,9 @@
     export let data;
 
     console.log(data)
+
+    // use data.anmie for list of animes e.g
+    console.log(data.animes);
 </script>
 
 
--- src/routes/list/+page.ts
+++ src/routes/list/+page.ts
@@ -5,15 +5,31 @@ export const load: Load = async ({ params, fetch, parent }) => {
     // Get the Supabase client from the context
     const { supabase, session} = await parent();
 
-    // Fetch data from Supabase
+    // get animes
     const { data, error } = await supabase
-        .from('anime')
+        .from('animes')
         .select('*')
 
     if (error) {
         console.error(error);
         return { error };
     }
+
+    let responseData = {
+        animes: data
+    }
+
+    console.log(responseData)
+
+    responseData.animes.forEach(async (anime) => {
+            const response = await supabase.from("studios").select("*").eq("id", anime.studio);
+
+            anime.studio = response.data[0]
+    
+            if(error){
+                console.error(error);
+            }
+    });
 
     return { animes : data };
 };
