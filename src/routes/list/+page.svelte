0a6bf36b2f82b9b075956eeb04fd978d7d4a82a0
Maurice <170624038+N3r0nX@users.noreply.github.com>
2024-06-04 19:57:11 +0200

    List frontend anfang

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -5,7 +5,21 @@
     $: console.log(data.animes[0].studio.name)
 </script>
 
-{#each data.animes as anime }
-    <div style="background-color: #f0f;">{anime.studio.name}</div>
-    <input type="text" bind:value={anime.studio.name} />
-{/each}
+
+
+
+<body class="bg-gray-900 text-white">
+    <div class="container mx-auto p-4">
+        <div class="mb-4 p-4 bg-gray-800 rounded">
+            <textarea class="w-full h-24 p-2 bg-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Feld mit Text drin"></textarea>
+        </div>
+        <div class="space-y-2">
+            <!-- Repeat this div for each entry -->
+            
+            {#each data.animes as anime }
+            <div class="p-2 bg-gray-800 rounded border border-gray-700">{anime.studio.name} </div>
+            <input class="p-2 bg-gray-800 rounded border border-gray-700" type="text" bind:value={anime.studio.name} />
+            {/each}
+        </div>
+    </div>
+</body>
